33ee77e992c8c41306937a16ee35d69f216bc089
Chris Hanretty <chris.hanretty@gmail.com>
2025-01-05 18:31:57 +0000

    Added the Excel output

--- R/008_prepare_both_waves_outputs.R
+++ R/008_prepare_both_waves_outputs.R
@@ -105,7 +105,7 @@ saveRDS(shiny,
         file = here::here("shiny_pairwise_mps",
                           "shiny_theta_iters.rds"))
 
-### Now something for export, not the full Excel version
+### Now something for export
 
 ###
 dat2 <- dat |>
@@ -126,17 +126,129 @@ dat2 <- dat |>
                   cult_lo = value_lo_2,
                   cult_hi = value_hi_2)
 
-ggplot(dat2,
-       aes(x = econ_mean, xmin = econ_lo, xmax = econ_hi,
-           y = cult_mean, ymin = cult_lo, ymax = cult_hi)) +
-    geom_pointrange() +
-    facet_wrap(~Party.24) +
-    theme_bw()
-
 write.csv(dat2,
           file = here::here("outputs",
                             "summary_statistics_file.csv"),
           row.names = FALSE)
+
+### Create something prettier looking in Excel
+### Export as Excel
+
+### (1) Create the workbook
+wb <- createWorkbook()
+options("openxlsx.numFmt" = "0.0") # 1 decimal cases formating
+
+modifyBaseFont(wb, fontSize = 10, fontName = "Arial Narrow")
+
+### Create an initial sheet
+addWorksheet(wb, sheetName = "Frontispiece", gridLines = FALSE)
+
+if (!file.exists(here::here("outputs", "rhul_logo.jpg"))) {
+    download.file("https://intranet.royalholloway.ac.uk/staff/assets/img/brand-toolkit/logo-small-london-cmyk.jpg",
+                  destfile = here::here("outputs", "rhul_logo.jpg"),
+                  method = "wget", extra = "--no-check-certificate")
+}
+
+
+insertImage(wb,
+            "Frontispiece",
+            here::here("outputs", "rhul_logo.jpg"),
+            width = 3, height = 1.5,
+            startRow = 2, startCol = 1)
+
+if (!file.exists(here::here("outputs", "survation_logo.png"))) {
+    download.file("https://cdn.survation.com/wp-content/theme/images/logo.png",
+                  destfile = here::here("outputs", "survation_logo.png"))
+}
+   
+insertImage(wb,
+            "Frontispiece",
+            width = 3, height = .6,
+            here::here("outputs", "survation_logo.png"),
+            startRow = 4, startCol = 5)
+
+insertImage(wb,
+            "Frontispiece",
+            width = 3, height = 1.5,
+            here::here("outputs", "ukice_logo.png"),
+            startRow = 2, startCol = 10)
+
+
+### Add a table
+
+dat3 <- dat2 |>
+    dplyr::select(Name = DisplayName,
+                  `Wikidata identifier` = wikidata,
+                  `Constituency (2019-2024 parliament)` = PCON22NM,
+                  `Constituency code (2019-2024 parliament)` = PCON22CD,
+                  `Party (2019-2024 parliament)` = Party.22,
+                  `Constituency (2024- parliament)` = PCON24NM,
+                  `Constituency code (2024- parliament)` = PCON24CD,
+                  `Party (2024- parliament)` = Party.24,
+                  Value = econ_mean,
+                  `Could be as low as` = econ_lo,
+                  `Could be as high as` = econ_hi)
+
+
+tab <- tribble(~Column, ~Explanation,
+               "Name", "MP's name in format FirstName LASTNAME",
+               "Wikidata identifier", "Wikidata identifier code for the MP (hidden)",
+               "Constituency (2019-2024 parliament)", "MP's old constituency",
+               "Constituency code (2019-2024 parliament)", "ONS identifier code for the constituency (hidden)",
+               "Party (2019-2024 parliament)", "Party as of autumn 2023",
+               "Constituency (2024- parliament)", "MP's current constituency",
+               "Constituency code (2024- parliament)", "ONS identifier code for the constituency (hidden)",
+               "Party (2024- parliament)", "Party as of autumn 2024",
+               "Value", "Value from 0-100, where higher numbers mean MP is more right-wing",
+               "Could be as low as", "Lower end of a credible interval. There's a very high probability (95%) that the MP's true value is greater than this number",
+               "Could be as high as", "Upper end of a credible interval. There's a very high probability (95%) that the MP's true value is lower than this number") |>
+    as.data.frame()
+
+writeDataTable(wb, sheet = 1, tab, startRow = 10)
+
+setColWidths(wb, 1, 1, 
+             widths = 20)
+
+### Create the data worksheet
+addWorksheet(wb, sheetName = "MP values", gridLines = FALSE)
+freezePane(wb, sheet = 2, firstRow = TRUE, firstCol = FALSE)
+
+writeDataTable(wb, sheet = 2, dat3, startRow = 1)
+
+### Hide column 2 and 4 and 7 (all codes)
+setColWidths(wb, 2, 2, 
+             widths = 8.43, hidden = TRUE)
+setColWidths(wb, 2, 4, 
+             widths = 8.43, hidden = TRUE)
+setColWidths(wb, 2, 7, 
+             widths = 8.43, hidden = TRUE)
+
+### Name, constituency name
+setColWidths(wb, 2, 1, 
+             widths = 24, hidden = FALSE)
+setColWidths(wb, 2, 3, 
+             widths = 24, hidden = FALSE)
+setColWidths(wb, 2, 6, 
+             widths = 24, hidden = FALSE)
+
+### party column
+setColWidths(wb, 2, 5, 
+             widths = 18, hidden = FALSE)
+setColWidths(wb, 2, 8, 
+             widths = 18, hidden = FALSE)
+
+### could be... columns
+setColWidths(wb, 2, 10, 
+             widths = 24, hidden = FALSE)
+setColWidths(wb, 2, 11, 
+             widths = 24, hidden = FALSE)
+
+### 
+### Export it
+
+saveWorkbook(wb,
+             here::here("outputs", "mpsleftright_excel.xlsx"),
+             overwrite = TRUE)
 
 ### Get positions in 2023
 dat <- readRDS(here::here("working", "bothwaves_thetas.rds"))
